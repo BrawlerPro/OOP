15f2317b6686265f873cdd04cd5c062c12c3f541
zeinalov.rr <zeinalov.rr@dozen.mephi.ru>
2024-10-17 01:33:00 +0300

    Up

--- Lab1/cmake-build-debug/Dialogue/CMakeFiles/Dialogue.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Dialogue.
-# This may be replaced when dependencies are built.
--- Lab1/cmake-build-debug/Tests/Tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/mnt/c/Users/rasul/CLionProjects/OOP/Lab1/cmake-build-debug/Tests/Tests[1]_tests.cmake")
-  include("/mnt/c/Users/rasul/CLionProjects/OOP/Lab1/cmake-build-debug/Tests/Tests[1]_tests.cmake")
-else()
-  add_test(Tests_NOT_BUILT Tests_NOT_BUILT)
-endif()
